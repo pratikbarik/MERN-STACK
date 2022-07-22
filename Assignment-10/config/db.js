7f6e035a9ff7f6c008cc1ca443df4696f986f9b0
pratikbarik <108912464+pratikbarik@users.noreply.github.com>
2022-07-22 10:21:15 +0530

    Delete Assignment-10/config directory

--- Assignment-10/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-exports.dbConn = ()=>{
-    const dbURL = "mongodb+srv://pratikbarik:pratik16181@cluster0.3gvef.mongodb.net/Mern_data?retryWrites=true&w=majority"
-mongoose.connect(dbURL,{useNewUrlParser:true,useUnifiedTopology:true}).then((result)=>{
-    console.log(`Database connected`)
- }).catch((err)=>console.log(err));
-}
